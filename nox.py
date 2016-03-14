09173758ac82d333a8fbee7bd3ac9a281484c625
Jon Wayne Parrott <jon.wayne.parrott@gmail.com>
2016-03-14 11:19:33 -0700

    Adding nox sessions to run tests on jenkins and travis

--- nox.py
+++ nox.py
@@ -1,7 +1,19 @@
 from glob import glob
+import shutil
+
+import nox
 
 REPO_TOOLS_REQ =\
     'git+https://github.com/GoogleCloudPlatform/python-repo-tools.git'
+
+DIRS = [
+    '1-hello-world',
+    '2-structured-data',
+    '3-binary-data',
+    '4-auth',
+    '5-logging',
+    '6-pubsub',
+    '7-gce']
 
 
 def session_reqcheck(session):
@@ -21,3 +33,24 @@ def session_lint(session):
     session.run(
         'flake8', '--exclude=env,.nox,._config.py',
         '--import-order-style=google', '.')
+
+
+@nox.parametrize('dir', DIRS)
+def session_run_tests(session, dir=None, toxargs=None):
+    """Run all tests for all directories (slow!)"""
+    session.chdir(dir)
+    session.run('tox', *(toxargs or []))
+
+
+@nox.parametrize('dir', DIRS)
+def session_travis(session, dir=None):
+    """On travis, only run the py3.4 and cloudsql tests."""
+    if dir == '1-hello-world':
+        session_run_tests(
+            session, dir=dir, toxargs=['-e', 'lint'])
+    else:
+        shutil.copy('config.py', dir)
+        session_run_tests(
+            session,
+            dir=dir,
+            toxargs=['-e', 'py34', '--', '-k', 'cloudsql'])
