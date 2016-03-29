4fc308d270eae3ad2603e993e577dbd183ba2990
Sharif Salah <sharif.salah@gmail.com>
2016-03-29 16:16:00 +0100

    Removed MongoDB library for application.

--- 2-structured-data/config.py
+++ 2-structured-data/config.py
@@ -22,7 +22,7 @@ You can create and manage projects at https://console.developers.google.com
 SECRET_KEY = 'secret'
 
 # There are three different ways to store the data in the application.
-# You can choose 'datastore', 'cloudsql', or 'mongodb'. Be sure to
+# You can choose 'datastore', or 'cloudsql'. Be sure to
 # configure the respective settings for the one you choose below.
 # You do not have to configure the other data backends. If unsure, choose
 # 'datastore' as it does not require any additional configuration.
