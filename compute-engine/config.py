31a2385efaf279dd9eaee410fd42fbafe458730c
Sharif Salah <sharif.salah@gmail.com>
2016-04-07 20:31:33 +0100

    Mapped project ID to bucket name in Compute Engine version of Bookshelf

--- compute-engine/config.py
+++ compute-engine/config.py
@@ -48,6 +48,6 @@ SQLALCHEMY_DATABASE_URI = \
 #
 # You can adjust the max content length and allow extensions settings to allow
 # larger or more varied file types if desired.
-CLOUD_STORAGE_BUCKET = 'your-bucket-name'
+CLOUD_STORAGE_BUCKET = PROJECT_ID
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
