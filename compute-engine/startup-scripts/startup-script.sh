fc737da27f0438b98de57c76c078ea167abede3b
Sharif Salah <sharif.salah@gmail.com>
2016-04-08 14:32:48 +0100

    Removed various unused files. Added a sed command to the startup script to replace the project ID in config.py

--- compute-engine/startup-scripts/startup-script.sh
+++ compute-engine/startup-scripts/startup-script.sh
@@ -44,9 +44,11 @@ export HOME=/root
 git config --global credential.helper gcloud.sh
 git clone https://source.developers.google.com/p/$PROJECTID /opt/app
 
+sed -i s/your-project-id/"$PROJECTID"/ /opt/app/compute-engine/config.py
+
 # Install app dependencies
-virtualenv /opt/app/5-compute-engine/env
-/opt/app/5-compute-engine/env/bin/pip install -r /opt/app/5-compute-engine/requirements.txt
+virtualenv /opt/app/compute-engine/env
+/opt/app/compute-engine/env/bin/pip install -r /opt/app/compute-engine/requirements.txt
 
 # Make sure the pythonapp user owns the application code
 chown -R pythonapp:pythonapp /opt/app
@@ -55,14 +57,14 @@ chown -R pythonapp:pythonapp /opt/app
 # applicaiton.
 cat >/etc/supervisor/conf.d/python-app.conf << EOF
 [program:pythonapp]
-directory=/opt/app/5-compute-engine
-command=/opt/app/5-compute-engine/env/bin/gunicorn main:app --bind 0.0.0.0:8080
+directory=/opt/app/compute-engine
+command=/opt/app/compute-engine/env/bin/gunicorn main:app --bind 0.0.0.0:8080
 autostart=true
 autorestart=true
 user=pythonapp
 # Environment variables ensure that the application runs inside of the
 # configured virtualenv.
-environment=VIRTUAL_ENV="/opt/app/env/5-compute-engine",PATH="/opt/app/5-compute-engine/env/bin",\
+environment=VIRTUAL_ENV="/opt/app/env/compute-engine",PATH="/opt/app/compute-engine/env/bin",\
     HOME="/home/pythonapp",USER="pythonapp"
 stdout_logfile=syslog
 stderr_logfile=syslog
