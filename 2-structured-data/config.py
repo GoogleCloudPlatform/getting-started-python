83ec2250123f09789067a79d7cb21fce7b5abf9f
Ryan Matsumoto <ryanmats@google.com>
2016-10-25 13:37:42 -0700

    Moved port 3306 comment to proxy section

--- 2-structured-data/config.py
+++ 2-structured-data/config.py
@@ -50,9 +50,10 @@ CLOUDSQL_CONNECTION_NAME = 'your-cloudsql-connection-name'
 #
 #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
 #
-# Alternatively, you could use a local MySQL instance for testing.
 # Port 3306 is the standard MySQL port. If you need to use a different port,
 # change the 3306 to a different port number.
+
+# Alternatively, you could use a local MySQL instance for testing.
 LOCAL_SQLALCHEMY_DATABASE_URI = (
     'mysql+pymysql://{user}:{password}@127.0.0.1:3306/{database}').format(
         user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
@@ -60,8 +61,6 @@ LOCAL_SQLALCHEMY_DATABASE_URI = (
 
 # When running on App Engine a unix socket is used to connect to the cloudsql
 # instance.
-# Port 3306 is the standard MySQL port. If you need to use a different port,
-# change the 3306 to a different port number.
 LIVE_SQLALCHEMY_DATABASE_URI = (
     'mysql+pymysql://{user}:{password}@127.0.0.1:3306/{database}'
     '?unix_socket=/cloudsql/{connection_name}').format(
