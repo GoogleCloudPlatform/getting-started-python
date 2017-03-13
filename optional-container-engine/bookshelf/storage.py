3b81503ab271681661bb2f90418c45e146bf9344
Rodrigo Queiro <rodrigoq@google.com>
2017-03-13 07:50:18 +0100

    Use bucket() instead of get_bucket()
    This avoids an unnecessary request, and means that the client will work
    when using the default "Storage Object Creator" role for a Cloud service
    account.

--- optional-container-engine/bookshelf/storage.py
+++ optional-container-engine/bookshelf/storage.py
@@ -57,7 +57,7 @@ def upload_file(file_stream, filename, content_type):
     filename = _safe_filename(filename)
 
     client = _get_storage_client()
-    bucket = client.get_bucket(current_app.config['CLOUD_STORAGE_BUCKET'])
+    bucket = client.bucket(current_app.config['CLOUD_STORAGE_BUCKET'])
     blob = bucket.blob(filename)
 
     blob.upload_from_string(
