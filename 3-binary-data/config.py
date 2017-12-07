b39751f256eb289c1b5025fc781f7fd3650540a2
Jon Wayne Parrott <jonwayne@google.com>
2017-12-07 13:09:50 -0800

    Fix bad config

--- 3-binary-data/config.py
+++ 3-binary-data/config.py
@@ -32,7 +32,7 @@ DATA_BACKEND = 'datastore'
 
 # Google Cloud Project ID. This can be found on the 'Overview' page at
 # https://console.developers.google.com
-PROJECT_ID = 'python-docs-samples-tests'
+PROJECT_ID = 'your-project-id'
 
 # CloudSQL & SQLAlchemy configuration
 # Replace the following values the respective values of your Cloud SQL
@@ -90,6 +90,6 @@ MONGO_URI = 'mongodb://user:password@host:27017/database'
 #
 # You can adjust the max content length and allow extensions settings to allow
 # larger or more varied file types if desired.
-CLOUD_STORAGE_BUCKET = 'python-docs-samples-tests'
+CLOUD_STORAGE_BUCKET = 'your-bucket-name'
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
