cb1b422d460acf4ecbdb2feee8d6f1fbb955f86d
Charles Engelke <engelke@google.com>
2019-08-22 12:35:40 -0700

    Added tests for background tasks

--- background/app/main_test.py
+++ background/app/main_test.py
@@ -11,3 +11,83 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import json
+import main
+import pytest
+
+
+class MockFirestoreClient():
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def collection(self, name):
+        return self
+
+    def stream(self):
+        return [self]
+
+    def to_dict(self):
+        return {
+            'Original': 'This is a test sentence',
+            'Language': 'de',
+            'Translated': 'This should be in German in a real environment',
+            'OriginalLanguage': 'en',
+        }
+
+
+class MockPublisherClient():
+    def __init__(self, *args, **kwargs):
+        self.messages = []
+
+    def publish(self, topic_name, json_message):
+        message = json.loads(json_message)
+        self.messages.append({
+            'topic': topic_name,
+            'message': message,
+        })
+
+
+@pytest.yield_fixture
+def db():
+    client = MockFirestoreClient()
+    yield client
+
+
+@pytest.yield_fixture
+def publisher():
+    client = MockPublisherClient()
+    yield client
+
+
+
+def test_index(db, publisher):
+    main.app.testing = True
+    main.db = db
+    main.publisher = publisher
+    client = main.app.test_client()
+
+    r = client.get('/')
+    assert r.status_code == 200
+    response_text = r.data.decode('utf-8')
+    assert 'Text to translate' in response_text
+    assert 'This should be in German' in response_text
+
+
+def test_translate(db, publisher):
+    main.app.testing = True
+    main.db = db
+    main.publisher = publisher
+    client = main.app.test_client()
+
+    r = client.post('/request-translation', data={
+        'v': 'This is a test',
+        'lang': 'fr',
+    })
+
+    assert r.status_code < 400
+    assert len(publisher.messages) == 1
+    assert '/translate' in publisher.messages[0]['topic']
+    assert publisher.messages[0]['message']['Original'] == 'This is a test'
+    assert publisher.messages[0]['message']['Language'] == 'fr'
+
--- background/function/main_test.py
+++ background/function/main_test.py
@@ -11,3 +11,51 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import base64
+import json
+import main
+import pytest
+
+
+class MockTranslateClient():
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def translate(self, s, target_language='N/A'):
+        return {
+            'translatedText': 'This is the output string',
+            'detectedSourceLanguage': 'en',
+        }
+
+
+@pytest.yield_fixture
+def xlate():
+    client = MockTranslateClient()
+    yield client
+
+
+last_message = None
+def dummy_update(transaction, message):
+    global last_message
+    last_message = message
+
+
+def test_invocations(xlate):
+    main.update_database = dummy_update
+    main.xlate = xlate
+
+    event = {
+        'data': base64.b64encode(json.dumps({
+            'Original': 'My test message',
+            'Language': 'de',
+        }).encode('utf-8'))
+    }
+
+    main.translate_message(event, None)
+
+    assert last_message is not None
+    assert last_message['Original'] == 'My test message'
+    assert last_message['Language'] == 'de'
+    assert last_message['Translated'] == 'This is the output string'
+    assert last_message['OriginalLanguage'] == 'en'
