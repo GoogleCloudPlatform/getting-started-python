aa79fb73aa5c4cb62218e3234138288519862e3d
Charles Engelke <engelke@google.com>
2019-09-05 23:27:23 +0000

    Added service account for firestore testing

--- encrypt-secrets.sh
+++ encrypt-secrets.sh
@@ -17,7 +17,7 @@
 read -s -p "Enter password for encryption: " password
 echo
 
-tar cvf secrets.tar {service-account.json,config.py}
+tar cvf secrets.tar {service-account.json,firestore-service-account.json,config.py}
 openssl aes-256-cbc -k "$password" -in secrets.tar -out secrets.tar.enc
 rm secrets.tar
 
