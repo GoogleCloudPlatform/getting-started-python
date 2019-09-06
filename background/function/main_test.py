39fa0a1dc7a4f88d402f7e8a3cac69a4318a4cbe
Charles Engelke <engelke@google.com>
2019-09-06 13:28:47 -0700

    Update main_test.py

--- background/function/main_test.py
+++ background/function/main_test.py
@@ -19,7 +19,7 @@ import pytest
 
 from google.cloud import firestore
 
-os.environ['GOOGLE_CLOUD_PROJECT'] = os.environ.get('FIRESTORE_PROJECT_ID')
+os.environ['GOOGLE_CLOUD_PROJECT'] = FIRESTORE_PROJECT_ID
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.realpath(
     os.path.join(
         os.path.dirname(__file__),
