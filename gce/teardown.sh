3bafcdb93086e9fcf25a9d3a6f947dc6f3459e0b
Brent Shaffer <betterbrent@google.com>
2019-12-09 18:40:14 -0800

    ensure the app serves on port 80 on GCE

--- gce/teardown.sh
+++ gce/teardown.sh
@@ -21,5 +21,5 @@ ZONE=us-central1-f
 
 gcloud compute instances delete $MY_INSTANCE_NAME \
     --zone=$ZONE --delete-disks=all
-    
-gcloud compute firewall-rules delete default-allow-http-8080
+
+gcloud compute firewall-rules delete default-allow-http-80
