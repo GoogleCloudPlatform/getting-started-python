a4cf55d315bbc7f3ae196d72528296ef471e58e8
Brent Shaffer <betterbrent@google.com>
2019-12-09 18:32:05 -0800

    uses tutorial defaults

--- gce/deploy.sh
+++ gce/deploy.sh
@@ -15,8 +15,8 @@
 set -ex
 
 # [START getting_started_gce_create_instance]
-MY_INSTANCE_NAME="my-instance-name"
-ZONE=us-central1-f
+MY_INSTANCE_NAME="my-app-instance"
+ZONE=us-central1-a
 
 gcloud compute instances create $MY_INSTANCE_NAME \
     --image-family=debian-9 \
