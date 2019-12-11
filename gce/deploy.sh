377e2c64a63b0c2f3a269a0572b0aebf576b6a75
Charles Engelke <engelke@google.com>
2019-12-11 12:36:05 -0800

    On-ramp GCE sample (#205)

---
+++ gce/deploy.sh
@@ -0,0 +1,35 @@
+# Copyright 2019 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+# [START getting_started_gce_create_instance]
+MY_INSTANCE_NAME="my-app-instance"
+ZONE=us-central1-a
+
+gcloud compute instances create $MY_INSTANCE_NAME \
+    --image-family=debian-9 \
+    --image-project=debian-cloud \
+    --machine-type=g1-small \
+    --scopes userinfo-email,cloud-platform \
+    --metadata-from-file startup-script=startup-script.sh \
+    --zone $ZONE \
+    --tags http-server
+# [END getting_started_gce_create_instance]
+
+gcloud compute firewall-rules create default-allow-http-8080 \
+    --allow tcp:8080 \
+    --source-ranges 0.0.0.0/0 \
+    --target-tags http-server \
+    --description "Allow port 8080 access to http-server"
---
+++ gce/startup-script.sh
@@ -0,0 +1,49 @@
+# Copyright 2019 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Echo commands
+set -v
+
+# [START getting_started_gce_startup_script]
+# Install Stackdriver logging agent
+curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+sudo bash install-logging-agent.sh
+
+# Install or update needed software
+apt-get update
+apt-get install -yq git supervisor python python-pip
+pip install --upgrade pip virtualenv
+
+# Account to own server process
+useradd -m -d /home/pythonapp pythonapp
+
+# Fetch source code
+export HOME=/root
+git clone https://github.com/GoogleCloudPlatform/getting-started-python.git /opt/app
+
+# Python environment setup
+virtualenv -p python3 /opt/app/gce/env
+source /opt/app/gce/env/bin/activate
+/opt/app/gce/env/bin/pip install -r /opt/app/gce/requirements.txt
+
+# Set ownership to newly created account
+chown -R pythonapp:pythonapp /opt/app
+
+# Put supervisor configuration in proper place
+cp /opt/app/gce/python-app.conf /etc/supervisor/conf.d/python-app.conf
+
+# Start service via supervisorctl
+supervisorctl reread
+supervisorctl update
+# [END getting_started_gce_startup_script]
